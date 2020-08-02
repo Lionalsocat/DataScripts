27a22f9df197728e9d40bb5a4c120828544ef122
MonsterKraken <949931551@qq.com>
2020-08-02 16:13:43 +0800

    upload txt2xml and xml2txt!

---
+++ txt2xml.py
@@ -0,0 +1,79 @@
+#! /usr/bin/python
+# -*- coding:UTF-8 -*-
+# 上述为声明 编码形式为UTF-8
+
+import os, sys
+import glob
+from PIL import Image
+
+img_path = "./JPEGImages"       # 图像存储位置
+txt_path = "./labels"           # txt文件存储位置
+xml_path = "./Annotations"      # xml存储位置
+classes = ["", ""]              # 获得对应ID，要和txt格式中类别对应起来
+suffix = ".jpg"                 # 图片后缀名
+
+if not os.path.exists(xml_path):
+    os.makedirs(xml_path)
+    
+# 坐标转换   (x, y, w, h)-<归一化的中心点坐标和框的宽高>   转换为  （xmin, ymin, xmax, ymax)-<左上和右下坐标>
+def convert(x, y, w, h):
+    x_min = int(width * (float(x) - float(w)/2 ))
+    y_min = int(height * (float(y) - float(h)/2 ))
+    x_max = int(width * (float(x) + float(w)/2 ))
+    y_max = int(height * (float(y) + float(h)/2 ))
+    return (x_min, y_min, x_max, y_max)
+    
+img_Lists = glob.glob(img_path + '/*' + suffix)
+ 
+img_basenames = [] # e.g. 100.jpg
+for item in img_Lists:
+    img_basenames.append(os.path.basename(item))
+ 
+img_names = [] # e.g. 100
+for item in img_basenames:
+    temp1, temp2 = os.path.splitext(item)
+    img_names.append(temp1)
+ 
+for img in img_names:
+    im = Image.open((img_path + '/' + img + suffix))
+    width, height = im.size
+ 
+    # 打开图片对应的txt文件  并且按照每一行为一个元素存入 img_all_label 数组中！
+    img_all_label = open(txt_path + '/' + img + '.txt').read().splitlines()
+ 
+    # 在xml中写入基本信息
+    xml_file = open((xml_path + '/' + img + '.xml'), 'w')
+    xml_file.write('<annotation>\n')
+    xml_file.write('    <filename>' + str(img) + suffix + '</filename>\n')
+    xml_file.write('    <size>\n')
+    xml_file.write('        <width>' + str(width) + '</width>\n')
+    xml_file.write('        <height>' + str(height) + '</height>\n')
+    xml_file.write('        <depth>3</depth>\n')
+    xml_file.write('    </size>\n')
+    xml_file.write('    <segmented>0</segmented>\n')
+ 
+    # 在xml中写入位置信息
+    for img_each_label in img_all_label:
+        spt = img_each_label.split(' ') 
+        # 如果txt里面是以逗号‘，’隔开的，那么就改为spt = img_each_label.split(',')。
+        # spt中存储的是 {类别，x，y，w，h}
+            
+        xml_file.write('    <object>\n')
+        xml_file.write('        <name>'+classes[int(spt[0])]+'</name>\n')
+        xml_file.write('        <pose>Unspecified</pose>\n')
+        xml_file.write('        <truncated>0</truncated>\n')
+        xml_file.write('        <difficult>0</difficult>\n')
+        xml_file.write('        <bndbox>\n')
+        # 转换坐标         
+        box = convert(spt[1], spt[2], spt[3], spt[4])            
+        xml_file.write('            <xmin>' + str(box[0]) + '</xmin>\n')
+        xml_file.write('            <ymin>' + str(box[1]) + '</ymin>\n')
+        xml_file.write('            <xmax>' + str(box[2]) + '</xmax>\n')
+        xml_file.write('            <ymax>' + str(box[3]) + '</ymax>\n')
+        xml_file.write('        </bndbox>\n')
+        xml_file.write('    </object>\n')
+    xml_file.write('</annotation>')
+
+# 已生成的xml总数
+xml_num = len([lists for lists in os.listdir(xml_path) if os.path.isfile(os.path.join(xml_path, lists))])
+print(str(xml_num) + " file is done!")
---
+++ xml2txt.py
@@ -0,0 +1,69 @@
+import xml.etree.ElementTree as ET
+import os
+from os import listdir, getcwd
+from os.path import join
+from PIL import Image
+
+img_path = "./JPEGImages"       # 图像存储位置
+txt_path = "./labels"           # txt文件存储位置
+xml_path = "./Annotations"      # xml存储位置
+classes = ["", ""]              # 获得对应ID，要和xml格式中类别对应起来
+suffix = ".jpg"                 # 图片后缀名
+  
+if not os.path.exists(txt_path):
+	os.makedirs(txt_path)
+	
+# 坐标转换   （xmin, ymin, xmax, ymax)-<左上和右下坐标>   转换为   (x, y, w, h)-<归一化的中心点坐标和框的宽高>   
+def convert(size, box):
+	dw = 1. / (size[0])
+	dh = 1. / (size[1])
+	x = (box[0] + box[1]) / 2.0 
+	y = (box[2] + box[3]) / 2.0 
+	w = box[1] - box[0]
+	h = box[3] - box[2]
+	x = x * dw
+	w = w * dw
+	y = y * dh
+	h = h * dh
+	return (x, y, w, h)
+
+
+def convert_annotation(image_id):
+    in_file = open(xml_path + '/%s.xml' % (image_id),'rb')       # 输入文件xml
+    out_file = open(txt_path + '/%s.txt' % (image_id), 'w')      # 输出文件txt
+    
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+
+    # 如果xml中没有size字段，则从图片中读取w和h, 不然则从xml中读取
+    if size == None:      
+        img = Image.open(img_path + '/' + image_id + suffix)
+        w, h = img.size  
+        print('{}.xml缺失size字段, 读取{}{}图片得到对应 w：{} h：{}'.format(image_id, image_id,suffix, w, h))       
+    else:
+        w = int(size.find('width').text)
+        h = int(size.find('height').text)
+        
+    for obj in root.iter('object'):
+        # 获取类别名
+        cls = obj.find('name').text
+        if cls not in classes:
+            continue
+        # 查找类别对应的索引
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+            float(xmlbox.find('ymax').text))
+        bb = convert((w, h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+                
+if __name__=='__main__':   		
+	list_file = os.listdir(img_path)    
+	for file in list_file:
+		image_id = file.split('.')[0]
+		convert_annotation(image_id)
+		
+	# 已生成的txt总数
+	txt_num = len([lists for lists in os.listdir(txt_path) if os.path.isfile(os.path.join(txt_path, lists))])
+	print(str(txt_num)+" flie is done!")
